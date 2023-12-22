69ba4a67a909c9d56cfb2cd531f12251ed1b5f9d
Boussoualef Mohamed Amine <124268899+MohamedAmine4@users.noreply.github.com>
2023-12-22 04:01:48 +0100

    MAP

--- lib/screens/settings_screen.dart
+++ lib/screens/settings_screen.dart
@@ -8,16 +8,34 @@ import 'package:smart_waste_manager_mobile_app/screens/ville_collection.dart';
 import 'package:smart_waste_manager_mobile_app/widgets/confirmation_dialog_widget.dart';
 import 'package:smart_waste_manager_mobile_app/models/waste_bin.dart';
 import 'package:smart_waste_manager_mobile_app/screens/next_collection_screen.dart';
+import 'package:smart_waste_manager_mobile_app/widgets/widgetCllectMapInfo.dart';
 
 class SettingsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-
     List<WasteBin> wasteBins = [
-    WasteBin(id: 1, latitude: 37.7749, longitude: -122.4214, fillLevel: 0.98, nextCollectionDate: DateTime.now(), status: ''),
-    WasteBin(id: 2, latitude: 37.7839, longitude: -122.4214, fillLevel: 0.7, nextCollectionDate: DateTime.now(), status: ''),
-    WasteBin(id: 3, latitude: 37.7839, longitude: -122.4218, fillLevel: 0.3, nextCollectionDate: DateTime.now(), status: ''),
-  ];
+      WasteBin(
+          id: 1,
+          latitude: 37.7749,
+          longitude: -122.4214,
+          fillLevel: 0.98,
+          nextCollectionDate: DateTime.now(),
+          status: ''),
+      WasteBin(
+          id: 2,
+          latitude: 37.7839,
+          longitude: -122.4214,
+          fillLevel: 0.7,
+          nextCollectionDate: DateTime.now(),
+          status: ''),
+      WasteBin(
+          id: 3,
+          latitude: 37.7839,
+          longitude: -122.4218,
+          fillLevel: 0.3,
+          nextCollectionDate: DateTime.now(),
+          status: ''),
+    ];
 
     return Scaffold(
       appBar: AppBar(
@@ -31,7 +49,7 @@ class SettingsScreen extends StatelessWidget {
               title: Text('Notifications'),
               subtitle: Text('Activer/désactiver les notifications'),
               trailing: Switch(
-                value: true,  // Remplacez par la vraie valeur de la notification
+                value: true, // Remplacez par la vraie valeur de la notification
                 onChanged: (bool value) {
                   // Ajoutez ici la logique pour activer/désactiver les notifications
                 },
@@ -41,10 +59,13 @@ class SettingsScreen extends StatelessWidget {
               title: Text('Rappels'),
               subtitle: Text('Configurer les rappels de collecte'),
               onTap: () {
-                  Navigator.of(context).pop(); // Fermer la boîte de dialogue actuelle
+                Navigator.of(context)
+                    .pop(); // Fermer la boîte de dialogue actuelle
                 Navigator.push(
                   context,
-                   MaterialPageRoute(builder: (context) => NextCollectScreen(wasteBins: wasteBins)),
+                  MaterialPageRoute(
+                      builder: (context) =>
+                          NextCollectScreen(wasteBins: wasteBins)),
                 );
                 // Ajoutez ici la logique pour naviguer vers l'écran de configuration des rappels
               },
@@ -53,50 +74,61 @@ class SettingsScreen extends StatelessWidget {
               title: Text('VilleCollection'),
               subtitle: Text('Configurer les rappels de collecte'),
               onTap: () {
-                  Navigator.of(context).pop(); // Fermer la boîte de dialogue actuelle
+                Navigator.of(context)
+                    .pop(); // Fermer la boîte de dialogue actuelle
                 Navigator.push(
                   context,
                   // MaterialPageRoute(builder: (context) => NextCollectScreen(wasteBins: wasteBins)),
-                    MaterialPageRoute(builder: (context) => VilleCollection(wasteBins: wasteBins)),
+                  MaterialPageRoute(
+                      builder: (context) =>
+                          VilleCollection(wasteBins: wasteBins)),
                 );
                 // Ajoutez ici la logique pour naviguer vers l'écran de configuration des rappels
               },
             ),
-              ListTile(
+            ListTile(
               title: Text('Bienvenu'),
               subtitle: Text('Configurer les rappels de collecte'),
               onTap: () {
-                  Navigator.of(context).pop(); // Fermer la boîte de dialogue actuelle
+                Navigator.of(context)
+                    .pop(); // Fermer la boîte de dialogue actuelle
                 Navigator.push(
                   context,
                   // MaterialPageRoute(builder: (context) => NextCollectScreen(wasteBins: wasteBins)),
-                    MaterialPageRoute(builder: (context) => Bienvenu(wasteBins: wasteBins)),
+                  MaterialPageRoute(
+                      builder: (context) => Bienvenu(wasteBins: wasteBins)),
                 );
                 // Ajoutez ici la logique pour naviguer vers l'écran de configuration des rappels
               },
             ),
-              ListTile(
+            ListTile(
               title: Text('Bienvenu Telephone'),
               subtitle: Text('Configurer les rappels de collecte'),
               onTap: () {
-                  Navigator.of(context).pop(); // Fermer la boîte de dialogue actuelle
+                Navigator.of(context)
+                    .pop(); // Fermer la boîte de dialogue actuelle
                 Navigator.push(
                   context,
                   // MaterialPageRoute(builder: (context) => NextCollectScreen(wasteBins: wasteBins)),
-                    MaterialPageRoute(builder: (context) => BienvenuPhone(wasteBins: wasteBins)),
+                  MaterialPageRoute(
+                      builder: (context) =>
+                          BienvenuPhone(wasteBins: wasteBins)),
                 );
                 // Ajoutez ici la logique pour naviguer vers l'écran de configuration des rappels
               },
             ),
-              ListTile(
+            ListTile(
               title: Text('Bienvenu Quartier'),
               subtitle: Text('Configurer les rappels de collecte'),
               onTap: () {
-                  Navigator.of(context).pop(); // Fermer la boîte de dialogue actuelle
+                Navigator.of(context)
+                    .pop(); // Fermer la boîte de dialogue actuelle
                 Navigator.push(
                   context,
                   // MaterialPageRoute(builder: (context) => NextCollectScreen(wasteBins: wasteBins)),
-                    MaterialPageRoute(builder: (context) => ChoixQuartier(wasteBins: wasteBins)),
+                  MaterialPageRoute(
+                      builder: (context) =>
+                          ChoixQuartier(wasteBins: wasteBins)),
                 );
                 // Ajoutez ici la logique pour naviguer vers l'écran de configuration des rappels
               },
@@ -105,24 +137,51 @@ class SettingsScreen extends StatelessWidget {
               title: Text('Ajouter Agent'),
               subtitle: Text('Configurer les rappels de collecte'),
               onTap: () {
-                  Navigator.of(context).pop(); // Fermer la boîte de dialogue actuelle
+                Navigator.of(context)
+                    .pop(); // Fermer la boîte de dialogue actuelle
                 Navigator.push(
                   context,
                   // MaterialPageRoute(builder: (context) => NextCollectScreen(wasteBins: wasteBins)),
-                    MaterialPageRoute(builder: (context) => AgentMatricule(wasteBins: wasteBins)),
+                  MaterialPageRoute(
+                      builder: (context) =>
+                          AgentMatricule(wasteBins: wasteBins)),
                 );
                 // Ajoutez ici la logique pour naviguer vers l'écran de configuration des rappels
               },
             ),
-              ListTile(
+            ListTile(
               title: Text('Statistique'),
               subtitle: Text('Configurer les rappels de collecte'),
               onTap: () {
-                  Navigator.of(context).pop(); // Fermer la boîte de dialogue actuelle
+                Navigator.of(context)
+                    .pop(); // Fermer la boîte de dialogue actuelle
                 Navigator.push(
                   context,
                   // MaterialPageRoute(builder: (context) => NextCollectScreen(wasteBins: wasteBins)),
-                    MaterialPageRoute(builder: (context) => Statistique(wasteBins: wasteBins)),
+                  MaterialPageRoute(
+                      builder: (context) => Statistique(wasteBins: wasteBins)),
+                );
+                // Ajoutez ici la logique pour naviguer vers l'écran de configuration des rappels
+              },
+            ),
+            ListTile(
+              title: Text('MapInfo'),
+              subtitle: Text('Configurer les rappels de collecte'),
+              onTap: () {
+                Navigator.of(context)
+                    .pop(); // Fermer la boîte de dialogue actuelle
+                Navigator.push(
+                  context,
+                  // MaterialPageRoute(builder: (context) => NextCollectScreen(wasteBins: wasteBins)),
+                  MaterialPageRoute(
+                      builder: (context) => CollectMapInfoWidget(
+                            distance: '5km',
+                            time: '5',
+                            totalPoubelle: 6,
+                            totalMaison: 7,
+                            totalEndommage: 2,
+                            score: 2,
+                          )),
                 );
                 // Ajoutez ici la logique pour naviguer vers l'écran de configuration des rappels
               },
